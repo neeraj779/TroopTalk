0b804cf5ad34e171510378c0041b3945f83610d4
Neeraj <neeraj79108@gmail.com>
2023-10-01 17:53:37 +0000

    Add 'python-decouple' library for managing environment variables

--- TroopTalk/settings.py
+++ TroopTalk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*9u$g@1i%51w)z=b@7v**+sh_07c%034@(xg41g)r$1mmn@@y'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
